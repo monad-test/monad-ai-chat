848e920910e2f98338e5a02404ae26b265878f92
ichiragkumar <zkichirag@gmail.com>
2025-09-24 20:05:22 +0530

    fixed ui issue

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import {
   http,
   WagmiProvider,
 } from "wagmi";
-import { sepolia,monadTestnet } from "wagmi/chains";
+import {monadTestnet } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@rainbow-me/rainbowkit/styles.css";
 
